76dae2f0fcb34bf348f96abe555023d05cf14e19
dwaynediddy <burrows.dwayne@icloud.com>
2021-01-05 14:49:25 +1000

    first

---
+++ src/components/Navbar.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <div>
+            <ul>
+                <li>
+                    Score
+                </li>
+                <li>
+                    Boss
+                </li>
+                <li>
+                    Player Profile
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+export default Navbar
