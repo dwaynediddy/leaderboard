0d8b0a6cc1a99df59d83a4bb8965d8a5e56856d2
dwaynediddy <burrows.dwayne@icloud.com>
2021-01-06 10:55:27 +1000

    added deaths

--- src/screens/Score.js
+++ src/screens/Score.js
@@ -9,20 +9,20 @@ const Score = () => {
         {value: 2, label: '2 Points'},
         {value: 3, label: '3 Points'}
     ]
-    const loot = [
-        {value: 1, label: '1 Point'}, 
-        {value: 2, label: '2 Points'},
-        {value: 3, label: '3 Points'}
+    const deaths = [
+        {value: 0, label: '0'},
+        {value: -1, label: '1'},
+        {value: -2, label: '2'},
+        {value: -3, label: '3'},
+        {value: -4, label: '4'},
+        {value: -5, label: '5'}
     ]
-    const time = [
-        {value: 1, label: '1 Point'}, 
-        {value: 2, label: '2 Points'},
-        {value: 3, label: '3 Points'}
-    ]
+
 
     const [selectedValue, setSelectedValue ] = useState(null)
     const [selectedLoot, setSelectedLoot ] = useState(null)
     const [selectedTime, setSelectedTime ] = useState(null)
+    const [selectedDeaths, setSelectedDeaths] = useState(null)
 
     const handleChange = obj => {
         setSelectedValue(obj)
@@ -33,6 +33,10 @@ const Score = () => {
     const handleTime = obj => {
         setSelectedTime(obj)
     }
+    const handleDeaths = obj => {
+        setSelectedDeaths(obj)
+    }
+    
 
     return (
         <>
@@ -47,19 +51,26 @@ const Score = () => {
                 How many points did you get for Loot:
                 <Select
                     value={selectedLoot}
-                    options={loot}
+                    options={option}
                     onChange={handleLoot}
                 />
                 How many points did you get for Time:
                 <Select
                     value={selectedTime}
-                    options={time}
+                    options={option}
                     onChange={handleTime}
+                />
+                How many times did you Die Noob:
+                <Select
+                    value={selectedDeaths}
+                    options={deaths}
+                    onChange={handleDeaths}
                 />
 
                 <br />
                 <b>Selected Value: </b>
-                <pre>{JSON.stringify(selectedValue, selectedTime, selectedLoot, null, 2)}</pre>
+
+                <pre>{JSON.stringify(selectedValue, selectedTime, selectedLoot, selectedDeaths, null, 2)}</pre>
                 
             </div>
         </>
