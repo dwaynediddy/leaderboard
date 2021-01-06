9b9e04211ff696432efa44202ec409f011bcb123
dwaynediddy <burrows.dwayne@icloud.com>
2021-01-06 10:46:14 +1000

    individual scoring worms

--- src/screens/Score.js
+++ src/screens/Score.js
@@ -9,11 +9,29 @@ const Score = () => {
         {value: 2, label: '2 Points'},
         {value: 3, label: '3 Points'}
     ]
+    const loot = [
+        {value: 1, label: '1 Point'}, 
+        {value: 2, label: '2 Points'},
+        {value: 3, label: '3 Points'}
+    ]
+    const time = [
+        {value: 1, label: '1 Point'}, 
+        {value: 2, label: '2 Points'},
+        {value: 3, label: '3 Points'}
+    ]
 
     const [selectedValue, setSelectedValue ] = useState(null)
+    const [selectedLoot, setSelectedLoot ] = useState(null)
+    const [selectedTime, setSelectedTime ] = useState(null)
 
     const handleChange = obj => {
         setSelectedValue(obj)
+    }
+    const handleLoot = obj => {
+        setSelectedLoot(obj)
+    }
+    const handleTime = obj => {
+        setSelectedTime(obj)
     }
 
     return (
@@ -28,18 +46,20 @@ const Score = () => {
                 />
                 How many points did you get for Loot:
                 <Select
-                    value={selectedValue}
-                    options={option}
+                    value={selectedLoot}
+                    options={loot}
+                    onChange={handleLoot}
                 />
                 How many points did you get for Time:
                 <Select
-                    value={selectedValue}
-                    options={option}
+                    value={selectedTime}
+                    options={time}
+                    onChange={handleTime}
                 />
 
                 <br />
                 <b>Selected Value: </b>
-                <pre>{JSON.stringify(selectedValue, null, 2)}</pre>
+                <pre>{JSON.stringify(selectedValue, selectedTime, selectedLoot, null, 2)}</pre>
                 
             </div>
         </>
