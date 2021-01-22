3549d46af5181d914e9d6383460c8e23f881897c
dwaynediddy <burrows.dwayne@icloud.com>
2021-01-22 15:59:43 +1000

    api and password handler

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,13 +2,37 @@ import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import '../styles/Login.css'
 
+async function loginUser(credentials) {
+    return fetch('http://localhost:5000/login', { 
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json'
+        },
+        body: JSON.stringify(credentials)
+    })
+        .then(data => data.json())
+}
+
+export default function login({ setToken }) {
+
+}
+
 const Login = ({ setToken }) => {
     const [username, setUsername] = useState()
     const [password, setPassword] = useState()
+
+    const handleSubmit = async e => {
+        e.preventDefault()
+        const token = await loginuser({ 
+            username,
+            password
+        })
+        setToken(token)
+    }
     return (
         <div className="login-wrapper">
             <h1>Please Login</h1>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label>
                     <p>Username</p>
                     <input type="text" onChange={e => setUsername(e.target.value)} />
